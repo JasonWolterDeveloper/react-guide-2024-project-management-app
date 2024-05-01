ab42243c12f36e06ce285ba6ad393a25986b88e9
JasonWolterDeveloper <143658631+JasonWolterDeveloper@users.noreply.github.com>
2024-05-01 14:30:42 -0500

    Button Class Implementation

--- src/components/NoProjectSelectedComponent.jsx
+++ src/components/NoProjectSelectedComponent.jsx
@@ -1,9 +1,15 @@
+import noProjectImage from '../assets/no-projects.png'
+import Button from './Button';
+
 export default function NoProjectSelectedComponent({ onAddNewProject }) {
     return (
-        <div>
-            <h1>No Project Selected</h1>
-            <p>Select a Project or get started with a new one</p>
-            <button onClick={onAddNewProject}>Create new project</button>
+        <div className="mt-24 text-center w-2/3">
+            <img src={noProjectImage} alt="An Empty Task List" className='w-16 h-16 object-contain mx-auto'/>
+            <h2 className='text-xl font-bold text-stone-500 my-4'>No Project Selected</h2>
+            <p className='text-stone-400 mb-4'>Select a Project or get started with a new one</p>
+            <p className='mt-8'>
+                <Button onClick={onAddNewProject}>Create new project</Button>
+            </p>
         </div>
     );
 }
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export default function ProjectsSidebar({ projectList, onAddNewProject, isAddingNewProject, onSelectProject }) {
 
     const addProjectHandler = () => {
@@ -8,7 +10,7 @@ export default function ProjectsSidebar({ projectList, onAddNewProject, isAdding
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">YOUR PROJECTS</h2>
             <div>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100" onClick={addProjectHandler} disabled={isAddingNewProject}>+ Add Project</button>
+                <Button onClick={addProjectHandler} disabled={isAddingNewProject}>+ Add Project</Button>
             </div>
             <ul>
                 {
